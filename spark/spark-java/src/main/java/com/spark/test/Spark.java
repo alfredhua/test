ad61fe11c66f7c71e7ff072df4a930db62f64375
guozhenhua7 <guozhenhua7@jd.com>
2021-06-03 20:23:00 +0800

    修改spark

---
+++ spark/spark-java/src/main/java/com/spark/test/Spark.java
@@ -0,0 +1,11 @@
+package com.spark.test;
+
+import org.apache.spark.SparkConf;
+
+public class Spark {
+
+    public static void main(String[] args) {
+        final SparkConf spark = new SparkConf().setAppName("Spark");
+
+    }
+}
