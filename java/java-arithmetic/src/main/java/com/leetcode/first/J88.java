77fec112ba1088b6c62d7eb94f7f1f0e4f1a6127
huazhenguo <hua_zhenguo@163.com>
2021-09-12 22:40:57 +0800

    J88

---
+++ java/java-arithmetic/src/main/java/com/leetcode/first/J88.java
@@ -0,0 +1,14 @@
+package com.leetcode.first;
+
+public class J88 {
+    public static void main(String[] args) {
+
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = 0;
+        int p2 = 0;
+
+    }
+
+}
