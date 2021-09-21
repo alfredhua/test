1005d71f2e418370f4f5b4fcf9c7af8d151f1db4
huazhenguo <hua_zhenguo@163.com>
2021-09-22 07:48:04 +0800

    j100

---
+++ java/java-arithmetic/src/main/java/com/leetcode/first/J100.java
@@ -0,0 +1,39 @@
+package com.leetcode.first;
+
+public class J100 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if ( p==null && q==null ){
+            return true;
+        }else if (p==null || q==null){
+            return false;
+        }else if(p.val!=q.val){
+            return false;
+        }else {
+            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        }
+
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val,TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
+
+
