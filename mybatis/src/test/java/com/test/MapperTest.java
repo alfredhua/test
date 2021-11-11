7ca8d37a8e16572fcae4c7e439999d46d16af192
guozhenhua7 <guozhenhua7@jd.com>
2021-11-11 15:16:28 +0800

    MapperTest

--- mybatis/src/test/java/com/test/MapperTest.java
+++ mybatis/src/test/java/com/test/MapperTest.java
@@ -3,6 +3,8 @@ package com.test;
 import com.test.mybatis.UserMapper;
 import com.test.mybatis.entity.User;
 import net.minidev.json.JSONArray;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,5 +32,26 @@ public class MapperTest {
         List<User> users = userMapper.selectUser();
         System.out.println(JSONArray.toJSONString(users));
 
+    }
+
+    @Autowired
+    SqlSession sqlSession;
+
+    //走Mybatis-Spring 中的SqlSessionTemplate 会反射Mybatis的SqlSession然后调用SelectList方法
+    @Test
+    public void test2(){
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = mapper.selectUser();
+    }
+
+
+    @Autowired
+    SqlSessionFactory sqlSessionFactory;
+
+    @Test
+    public void test3(){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper mapper =sqlSession.getMapper(UserMapper.class);
+        List<User> users = mapper.selectUser();
     }
 }
