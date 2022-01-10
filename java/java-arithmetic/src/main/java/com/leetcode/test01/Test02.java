7046df3eb7b734183cf537891f97c629fce12bcf
guozhenhua7 <guozhenhua7@jd.com>
2022-01-10 15:12:58 +0800

    idea

---
+++ java/java-arithmetic/src/main/java/com/leetcode/test01/Test02.java
@@ -0,0 +1,19 @@
+package com.leetcode.test01;
+
+public class Test02 {
+
+
+    public static void main(String[] args) {
+        int[] arr=new int[]{1,1,1,2,2,3};
+        int[] result = getNewArray(arr);
+        System.out.println(result);
+    }
+
+    public static int[] getNewArray(int[] array){
+        int[] result=new int[array.length];
+
+        return result;
+    }
+
+
+}
