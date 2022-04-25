83dfe88441f6d0a4c49cf7ab7ae2cea3eaf10fae
guozhenhua7 <guozhenhua7@jd.com>
2022-04-25 20:07:31 +0800

    test

---
+++ java/java-arithmetic/src/main/java/com/leetcode/tree/MidTree.java
@@ -0,0 +1,16 @@
+package com.leetcode.tree;
+
+/**
+ * @author guozhenhua
+ * @date 2022/4/25
+ */
+public class MidTree {
+
+    public static void main(String[] args) {
+
+    }
+    public void toMidTree(TreeNode treeNode){
+
+    }
+
+}
