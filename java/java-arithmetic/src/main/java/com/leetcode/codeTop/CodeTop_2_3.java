d4fdca246d833e5bfb127b4bcc30d38b04ed93d4
guozhenhua7 <guozhenhua7@jd.com>
2022-06-15 15:07:51 +0800

    codeTop_2_3

--- java/java-arithmetic/src/main/java/com/leetcode/codeTop/CodeTop_2_3.java
+++ java/java-arithmetic/src/main/java/com/leetcode/codeTop/CodeTop_2_3.java
@@ -2,10 +2,6 @@ package com.leetcode.codeTop;
 /**
  * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
  *
- *  
- *
- * 示例 1:
- *
  * 输入: s = "abcabcbb"
  * 输出: 3
  * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
@@ -22,10 +18,46 @@ package com.leetcode.codeTop;
  *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author guozhenhua
  * @date 2022/6/14
  */
 public class CodeTop_2_3 {
+
+    public static void main(String[] args) {
+        int abcabcbb = new CodeTop_2_3().lengthOfLongestSubstring("abcabcbb");
+        System.out.println(abcabcbb);
+        int bbbbb = new CodeTop_2_3().lengthOfLongestSubstring("bbbbb");
+        System.out.println(bbbbb);
+        int pwwkew = new CodeTop_2_3().lengthOfLongestSubstring("pwwkew");
+        System.out.println(pwwkew);
+
+        int a = new CodeTop_2_3().lengthOfLongestSubstring("au");
+        System.out.println(a);
+
+        int aa = new CodeTop_2_3().lengthOfLongestSubstring("aa");
+        System.out.println(aa);
+    }
+
+
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character,Integer> map=new HashMap<>();
+        int k =0;
+        int result = 0;
+        if (s.length()<2){
+            return  s.length();
+        }
+        for (int i=0;i<s.length();i++){
+            if (map.containsKey(s.charAt(i))){
+                k = map.get(s.charAt(i))+1;
+            }
+            map.put(s.charAt(i),i);
+            result = Math.max(result,i-k+1);
+        }
+        return result;
+    }
 
 }
