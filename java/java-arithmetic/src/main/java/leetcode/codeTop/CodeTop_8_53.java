fe5b8927baa30242f381d5fead89256bfcc142f7
guozhenhua7 <guozhenhua7@jd.com>
2022-08-16 17:43:20 +0800

    修改

--- java/java-arithmetic/src/main/java/leetcode/codeTop/CodeTop_8_53.java
+++ java/java-arithmetic/src/main/java/leetcode/codeTop/CodeTop_8_53.java
@@ -2,8 +2,26 @@ package leetcode.codeTop;
 
 public class CodeTop_8_53 {
 
+    public static void main(String[] args) {
+       int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(new CodeTop_8_53().maxSubArray(nums));
+    }
+
     public int maxSubArray(int[] nums) {
-        return 0;
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i =1;i<nums.length;i++){
+            if (dp[i - 1] > 0) {
+                dp[i] = dp[i - 1] + nums[i];
+            } else {
+                dp[i] = nums[i];
+            }
+        }
+        int res = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
     }
 
 
